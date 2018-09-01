c501736100cb9c87122915d43d717e7272b9ac74
sairam <sairam@sairam-PC>
2018-09-01 20:42:07 +0530

    Final Commit

---
+++ src/test/java/TestNG/addressbook.java
@@ -0,0 +1,37 @@
+package TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class addressbook {
+	WebDriver driver;
+	@BeforeMethod
+	public void launch() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	      driver = new ChromeDriver();
+	      driver.manage().window().maximize();
+	      driver.get("http://35.185.124.18:32768/addressbook/");
+	}
+  @Test
+  public void verifylogin() {
+	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
+	  driver.findElement(By.id("gwt-uid-5")).sendKeys("Testing");
+	  driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
+	  driver.findElement(By.id("gwt-uid-9")).sendKeys("9876543210");
+	  driver.findElement(By.id("gwt-uid-11")).sendKeys("devops@gmail.com");
+	  driver.findElement(By.id("gwt-uid-13")).sendKeys("01/11/1985");
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
+  }
+  
+  @AfterMethod
+ public void close() {
+	 driver.close();
+  }
+}
